c73354a8f52064ff2d25bdebf2c711010f456cdc
lijialiang <lijialiang@yy.com>
2021-07-26 09:42:32 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <p v-if="fruit === Fruit.apple">
+    apple
+  </p>
+  <p v-if="fruit === Fruit.banana">
+    banana
+  </p>
+</template>
+
+<script lang="ts" setup>
+enum Fruit {
+  apple = 0,
+  banana = 1
+}
+
+const fruit = 0
+</script>
